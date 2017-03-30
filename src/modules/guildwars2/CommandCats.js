61cc07895c993fc8686525c9085089d6b75540b7
Archomeda <archomeda@hotmail.com>
2017-03-31 01:46:03 +0200

    Add Super Adventure Box cat

--- src/modules/guildwars2/CommandCats.js
+++ src/modules/guildwars2/CommandCats.js
@@ -14,7 +14,7 @@ const bot = require('../../bot');
 
 
 // Hardcoded cats, lye pls
-const availableCats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32];
+const availableCats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33];
 
 class CommandCats extends Command {
     constructor(module) {
