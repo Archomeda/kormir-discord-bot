a1061d8713e8a5c454e9095fc481e4f756e65d49
Archomeda <archomeda@hotmail.com>
2017-08-09 07:17:34 +0200

    Add 3 new cats, hopefully they have the correct IDs

--- src/modules/guildwars2/commands/Cats.js
+++ src/modules/guildwars2/commands/Cats.js
@@ -11,7 +11,7 @@ const ApiBase = require('./ApiBase');
 
 
 // Hardcoded cats, lye pls
-const availableCats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 35];
+const availableCats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36, 37];
 
 
 class CommandCats extends ApiBase {
