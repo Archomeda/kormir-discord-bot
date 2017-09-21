220fdcb1e9eed7c6f64e8e23c9f02264d2b679ce
Archomeda <archomeda@hotmail.com>
2017-09-21 18:44:03 +0200

    Fix output of downloaded files

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/Archomeda/kormir-discord-bot/master/.docker/.docker-compose.yml
-wget https://raw.githubusercontent.com/Archomeda/kormir-discord-bot/master/.docker/.env
+wget https://raw.githubusercontent.com/Archomeda/kormir-discord-bot/master/.docker/docker-compose.yml -O docker-compose.yml
+wget https://raw.githubusercontent.com/Archomeda/kormir-discord-bot/master/.docker/.env -O .env
 
 mkdir config
 wget https://raw.githubusercontent.com/Archomeda/kormir-discord-bot/master/config/default.yml -O config/local.yml
